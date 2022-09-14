29acc06b1d266fe7a2ae2146fe8d44a62041d7be
Plugaru Roman <romaplugaru@gmail.com>
2022-09-14 09:03:29 +0300

    add sass

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import appContext from "./context";
 import ControlPanel from "./components/organism/ControlPanel";
 
